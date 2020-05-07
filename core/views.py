6c797d6b9690dad8caaaf1363150ee79c013ff82
paulojunior-mpa <paulo.link2012@gmail.com>
2020-05-07 11:20:12 -0300

    incluindo tela de autenticação

--- core/views.py
+++ core/views.py
@@ -1,10 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 # Create your views here.
+def login_user(request):
+    return render(request, 'login.html')
 
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "usuario ou senha invalido")
+    return redirect('/')
+
+@ login_required(login_url='/login/')
 def lista_evento(request):
+    user = request.user
     usuario = request.user
     evento = Evento.objects.all()
     dados = {'eventos':evento}
